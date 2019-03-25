574092bf1d9e9d1c32a1ac0a376cf0f6add460fb
Pablo Bellino <pbellino@gmail.com>
2019-03-25 02:16:29 -0300

    correcion estilo

--- modules/io_modules.py
+++ modules/io_modules.py
@@ -4,13 +4,15 @@
 import numpy as np
 import sys
 
+
 def read_bin_dt(filename):
     '''
-    Función para leer los archivos binarios grabados por el programa "intervaltime_MC"
+    Función para leer los archivoss grabados por el programa "intervaltime_MC"
 
     Cada dato es la cantidad de pulsos registrado en un dado dt.
-    Los archivos más viejos no tenían encabezado, mientras que los nuevos sí. 
-    El formato en que está grabado es entero sin signo de 32 bits con codificación big-endian
+    Los archivos más viejos no tenían encabezado, mientras que los nuevos sí.
+    El formato en que está grabado es entero sin signo de 32 bits con
+    codificación big-endian
 
     Parameters
     ----------
@@ -21,25 +23,25 @@ def read_bin_dt(filename):
     Returns
     -------
 
-    a : np.array 
+    a : np.array
         Datos leidos
     header: list of strings
         Encabezado el archivo
 
     '''
-    
-    # Tipo de dato: sin signo (por eso el 'u') 
+
+    # Tipo de dato: sin signo (por eso el 'u')
     #               32 bits (por eso el '4')
     #               'big-endian'(por eso el >)
     # https://docs.scipy.org/doc/numpy/reference/arrays.dtypes.html
     dt = np.dtype('>u4')
-    
+
     # Puntos que quiero leer de datos (-1 son todos)
     nread = -1
-    
+
     header = []
     try:
-        with open(filename,'rb') as f:
+        with open(filename, 'rb') as f:
             # Se fija si tiene encabezado
             header.append(f.readline().rstrip())
             if header[0].startswith(b'Nombre'):
@@ -50,7 +52,7 @@ def read_bin_dt(filename):
             else:
                 print('El archivo no tiene encabezado')
                 # Vuelvo al comienzo del archivo
-                f.seek(0,0)
+                f.seek(0, 0)
             # Continua leyendo los datos
             a = np.fromfile(f, dtype=dt, count=nread)
             # El primer dato se descarta
@@ -59,12 +61,16 @@ def read_bin_dt(filename):
         print('No se pudo leer el archivo: ' + filename)
         raise err
     except:
-        print('Se produjo un error inseperado al abrir/leer el archivo' + filename)        
+        print('Se produjo un error inseperado al abrir/leer el archivo'
+              + filename)
         sys.exit()
 
 
 def lee_dt_encabezado(encabezado):
-    """ Lee en el encabezado el intervalo dt con que se realizó la adquisición """
+    """
+    Lee en el encabezado el intervalo dt con que se realizó la adquisición
+
+    """
 
     dt = encabezado[4].decode('utf8').rsplit(':')[-1]
     dt = np.float(dt)
@@ -73,9 +79,9 @@ def lee_dt_encabezado(encabezado):
 
 
 def lee_bin_datos_dt(nombres):
-    """ 
+    """
     Lee los datos del archivo binario y obteiene el dt del encabezado
-    
+
     Parametros
     ----------
         nombres : lista de strings
@@ -88,8 +94,9 @@ def lee_bin_datos_dt(nombres):
             datos : lista de numpy array
                 Datos leidos de cada archivo dado en 'nombres'
             dt_base : lista de float
-                Intervalo temporal con que se hizo la medición para cada archivo
-    
+                Intervalo temporal con que se hizo la medición para cada
+                archivo
+
     """
 
     result = []
@@ -105,8 +112,28 @@ def lee_bin_datos_dt(nombres):
     return result
 
 
+def lee_historias_completas(nombre):
+    try:
+        with open(nombre, 'r') as f:
+            # Se lee el dt
+            for line in f:
+                if line.startswith('# Valor de dt'):
+                    dt = next(f)
+                    break
+            # Se leen todas las historias
+            data = np.loadtxt(nombre, skiprows=11)
+    except IOError as err:
+        print('No se pudo leer el archivo: ' + nombre)
+        raise err
+    except:
+        print('Se produjo un error inseperado al abrir/leer el archivo'
+              + nombre)
+        sys.exit()
+    return data, dt
+
+
 if __name__ == '__main__':
-    
+    """
     a, header = read_bin_dt('../datos/nucleo_01.D1.bin')
     for line in header:
         print(line)
@@ -115,3 +142,12 @@ if __name__ == '__main__':
     print(a[45000-1:45010-1])
     # Los últimos 10 datos
     print(a[-10:])
+    """
+
+    nombre = '../src/resultados/nucleo_01.D1.dat'
+
+    data, dt = lee_historias_completas(nombre)
+
+    print(dt)
+    print(data.shape)
+    print(data[:, 19])
