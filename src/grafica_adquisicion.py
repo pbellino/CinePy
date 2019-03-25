477627a83cea45d96e6ddaab791f8b97c22e3c9e
Pablo Bellino <pbellino@gmail.com>
2019-03-25 02:25:12 -0300

    correccion estilo

--- src/grafica_adquisicion.py
+++ src/grafica_adquisicion.py
@@ -12,6 +12,7 @@ from modules.io_modules import lee_bin_datos_dt
 
 plt.style.use('paper')
 
+
 def grafica_datos_agrupados(nombres, int_agrupar=None):
     """
     Grafica los datos de los archivos adquiridos
@@ -28,24 +29,25 @@ def grafica_datos_agrupados(nombres, int_agrupar=None):
         se calcula de tal manera de obetner un dt_agrupado de 1 seg
         (y el gráfico será la tasa de cuentas en cps)
         Si se da una lista, se usan distintos intervalos para cada archivo
-        
+
 
     """
     _results = lee_bin_datos_dt(nombres)
-   
+
     def _define_int_agrupar():
         """ Define los intervalos a agrupar en base al dado en el input """
 
         if int_agrupar is not None:
             if type(int_agrupar) is not list:
-                # Si sólo es un número, se hace que sea una lista con el mismo valor
-                # para todos los archivos leidos
+                # Si sólo es un número, se hace que sea una lista
+                # con el mismo valor para todos los archivos leidos
                 int_agrupar_it = [int_agrupar for _ in nombres]
             elif len(int_agrupar) != len(nombres):
                 # Si es una lista y no coinciden los tamaños, se sale
                 print('No coinciden los tamaños')
                 quit()
-            else: int_agrupar_it = int_agrupar
+            else:
+                int_agrupar_it = int_agrupar
         else:
             int_agrupar_it = []
             for _, dt_base in _results:
@@ -60,18 +62,19 @@ def grafica_datos_agrupados(nombres, int_agrupar=None):
     for j, result in enumerate(_results):
         dato = result[0]
         dt_base = result[1]
-        _data, dt_agrupado = agrupa_datos(dato, int_agrupar_it[j], dt_base) 
+        _data, dt_agrupado = agrupa_datos(dato, int_agrupar_it[j], dt_base)
         datos_agrupados.append(_data)
         # Construyo vector temporal
         dt_max = dt_agrupado * len(_data)
-        vec_temp.append(np.arange(dt_agrupado, dt_max + dt_agrupado, dt_agrupado))
+        vec_temp.append(np.arange(dt_agrupado, dt_max + dt_agrupado,
+                                  dt_agrupado))
 
     # Grafico
     fig1 = plt.figure(1)
-    ax1 = fig1.add_subplot(1,1,1)
-    
+    ax1 = fig1.add_subplot(1, 1, 1)
+
     for x, y in zip(vec_temp, datos_agrupados):
-        ax1.plot(x,y)
+        ax1.plot(x, y)
 
     ax1.set_xlabel('Tiempo [s]')
 
@@ -88,14 +91,13 @@ def grafica_datos_agrupados(nombres, int_agrupar=None):
         ax1.set_ylabel('Cuentas por dt')
         for nombre, dt in zip(nombres, dt_agrupado):
             str_legend.append(nombre.rsplit('/')[-1] + ' (dt={} s)'.format(dt))
-  
+
     ax1.legend(str_legend, loc='best')
 
     plt.show()
-    
+
 
 if __name__ == '__main__':
-
 
     # ---------------------------------------------------------------------------------
     # Parámetros de entrada
@@ -108,7 +110,7 @@ if __name__ == '__main__':
     # Intervalos para agrupar los datos adquiridos
     int_agrupar = 5000
     # ---------------------------------------------------------------------------------
-   
+
     grafica_datos_agrupados(nombres)
-    #grafica_datos_agrupados(nombres, [1000, 2000])
-    #grafica_datos_agrupados(nombres, int_agrupar)
+    # grafica_datos_agrupados(nombres, [1000, 2000])
+    # grafica_datos_agrupados(nombres, int_agrupar)
