0a217c4641b7d0e4ff7feb54d250425d39fe8a2b
Pablo Bellino <pbellino@gmail.com>
2019-05-19 21:56:19 -0300

    Se agrega análisis para dos exponenciales

--- src/analisis_alfa_feynman.py
+++ src/analisis_alfa_feynman.py
@@ -15,7 +15,7 @@ import sys
 sys.path.append('../')
 
 from modules.io_modules import lee_historias_completas, lee_fey
-from modules.funciones import alfa_feynman_lin_dead_time
+from modules.funciones import alfa_feynman_lin_dead_time, alfa_feynman_dos_exp
 
 
 def grafica_historias_afey(nombre):
@@ -84,6 +84,84 @@ def grafica_afey(nombres):
     # plt.show()
 
     return fig
+
+
+def ajuste_afey_2exp(tau, Y, std_Y):
+    """
+    Ajuste no lineal de la curva de alfa-Feynman
+
+    Se ajustan los datos de  Y vs tau con incerteza de stt_Y
+    Se utiliza el paquete lmfit para realizar el ajuste
+
+    """
+
+    def residual(params, tau, data=None, sigma=None):
+        parvals = params.valuesdict()
+        alfa = parvals['alfa']
+        amplitud = parvals['amplitud']
+        offset = parvals['offset']
+        alfa_2 = parvals['alfa_2']
+        amplitud_2 = parvals['amplitud_2']
+
+        model = alfa_feynman_dos_exp(tau, alfa, amplitud, offset,
+                                     alfa_2, amplitud_2)
+
+        if data is None:
+            return model
+        if sigma is None:
+            return model - data
+        return (model - data) / sigma
+
+    # Se definen los parámetros del ajuste
+    params = Parameters()
+    params.add('alfa', value=300, min=0)
+    params.add('amplitud', value=1, min=0)
+    params.add('offset', value=1)
+    params.add('alfa_2', value=2000, min=0)
+    params.add('amplitud_2', value=1, min=0)
+    # Se define la minimización
+    minner = Minimizer(residual, params,
+                       fcn_args=(tau,),
+                       fcn_kws={'data': Y, 'sigma': std_Y}
+                       )
+    # Se realiza la minimización
+    # Se puede usar directamente la función minimize como wrapper de Minimizer
+    result = minner.minimize(method='leastsq')
+    report_fit(result)
+
+    best_fit = Y + result.residual * std_Y
+
+    fig, (ax0, ax1) = plt.subplots(2, sharex=True,
+                                   gridspec_kw={'height_ratios': [3, 1]},
+                                   )
+
+    ax0.errorbar(tau, Y, yerr=std_Y, fmt='k.', elinewidth=4,
+                 label='measurement')
+    ax0.plot(tau, best_fit, 'r', zorder=3, label='fit')
+    ax0.set_ylabel(r'$Y(\tau)$')
+
+    # Invierto el orden de las leyendas
+    handles, labels = ax0.get_legend_handles_labels()
+    ax0.legend(handles[::-1], labels[::-1], loc='best')
+
+    ax1.plot(tau, result.residual, 'k.')
+    ax1.set_xlabel(r'$\tau$ [ms]')
+    ax1.set_ylabel(r'Residuals')
+    fig.subplots_adjust(hspace=0.1)
+
+    # Graficación del histograma de los residuos
+    fig2, ax3 = plt.subplots(1, 1)
+    ax3.hist(result.residual, bins=15, density=True, label='Residuals')
+    res_mean = np.mean(result.residual)
+    res_std = np.std(result.residual)
+    x = np.linspace(norm.ppf(0.001), norm.ppf(0.999), 500)
+    ax3.plot(x, norm.pdf(x, loc=res_mean, scale=res_std),
+             'r-', lw=5, alpha=0.6, label='Normal pdf')
+    # Invierto el orden de las leyendas
+    handles, labels = ax3.get_legend_handles_labels()
+    ax3.legend(handles[::-1], labels[::-1], loc='best')
+
+    return None
 
 
 def ajuste_afey(tau, Y, std_Y):
@@ -168,6 +246,7 @@ def lee_Nk(nombre):
     Nk = np.loadtxt(nombre, dtype=np.uint32)
     return Nk
 
+
 if __name__ == '__main__':
 
     # Carpeta donde se encuentra este script
@@ -202,8 +281,8 @@ if __name__ == '__main__':
     Nk = lee_Nk(nombre.rstrip('fey') + 'Nk')
     var_teo = teo_variance_berglof(Y, Nk)
 
-    # ajuste_afey(tau, Y, np.sqrt(var_teo/num_hist))
-    ajuste_afey(tau, Y, std_Y)
+    # ajuste_afey(tau, Y, std_Y)
+    ajuste_afey_2exp(tau, Y, std_Y)
     fig8, ax8 = plt.subplots(1, 1)
     ax8.plot(tau, num_hist*std_Y**2, '.', label='Estimated variance')
     ax8.set_xlabel(r'$\tau$ [ms]')
