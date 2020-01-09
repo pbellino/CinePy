2ec6339a971790494eb22fc235a165ed2601d066
pbellino <pbellino@gmail.com>
2020-01-09 16:56:41 -0300

    Se termina de escribir la función para hacer el procesamiento de alfa-rossi en todos los detectores

--- src/alfa_rossi.py
+++ src/alfa_rossi.py
@@ -94,7 +94,9 @@ def arossi_una_historia_I(data, dt_s, dtmax_s, tb):
     P_trigger = []
     for i, trigger in enumerate(data_ok):
         # Busco el bloque que voy a binear para este trigger
-        i_max = np.searchsorted(data, trigger + dtmax, side='right')
+        # Es importante que sea side=`left` para evitar que si existe un
+        # pulso en dtmax sea contado como un bin extra
+        i_max = np.searchsorted(data, trigger + dtmax, side='left')
         # Construyo el bloque y fijo t=0 en el trigger
         data_bin = data[i:i_max] - data[i]
         # Cuento los pulsos en cada bin
@@ -103,7 +105,7 @@ def arossi_una_historia_I(data, dt_s, dtmax_s, tb):
         p_hist[0] -= 1
         # Como np.bincount binea sólo hasta np.amax()+1, completo con
         # ceros el resto de los bines (hasta llenar los N_bin)
-        n_pad = np.int64(N_bin-p_hist.size)  # Cantidad de ceros que se agregan
+        n_pad = np.int(N_bin - p_hist.size)  # Cantidad de ceros que se agregan
         p_hist_completa = np.pad(p_hist, (0, n_pad), mode='constant')
         P_trigger.append(p_hist_completa)
     P_trigger = np.asarray(P_trigger)
@@ -116,7 +118,7 @@ def arossi_una_historia_I(data, dt_s, dtmax_s, tb):
 
 
 def arossi_serial(data_bloques_undet, dt_s, dtmax_s, tb):
-    """ Función en serie, sólo para debugg """
+    """ Función en serie, sólo para debugg (sólo para un detector)"""
     a = []
     for data in data_bloques_undet:
         a.append(arossi_una_historia_I(data, dt_s, dtmax_s, tb))
@@ -129,19 +131,56 @@ def wrapper_arossi_una_historia_I(arg_tupla):
     return arossi_una_historia_I(data, dt_s, dtmax_s, tb)
 
 
-def arossi_historias_undet(data_bloques_undet, dt_s, dtmax_s, tb):
+def arossi_historias(data_bloques, dt_s, dtmax_s, tb):
     """
-    TODO
+    Procesamiento de alfa-Rossi para todos los detectores.
+
+    Los cálculos por detector se hacen en serie. Los cálculos para las
+    historia están paralelizados con `multiprocessing`.
+
+    Parámetros
+    ----------
+        data_bloques : list of numpy ndarray
+            Cada elemento de la lista está compuesto por un array con todas
+            las historias. Es obtenido a partir de `alfa_rossi_preprocesado.py`
+        dt_s : double [segundos]
+            Duración de cada intervalo del histograma.
+        dtmax_s : double [segundos]
+            Tiempo total del histograma (N_bin = dtmax_s / dt_s)
+        tb : double [segundos]
+            Tiempo que dura cada pulso del contador
+
+    Resultados
+    ----------
+        results_detectores : lista de numpy ndarray
+            Cada elemento de la lista corresponde al resultado de un detector.
+            Cada elemento contiene a las salidas de la función
+            `wrapper_arossi_una_historia_I`:
+                - P_historia : numpy array
+                - R_historia : tupla (R_promedio, R_desvío)
+                - N_triggers : int
+                - P_trigger : list of list of numpy array
+            Para más detalle, ver la función `arossi_una_historia_I`
+
     """
     # Cantidad de procesadores disponibles
-    num_proc = mp.cpu_count()
-    pool = mp.Pool(processes=num_proc)
-    print('Se utilizan {} procesos'.format(num_proc))
-    # Construyo el argumento del wrapper en forma de tupla
-    argumentos_wrapper = zip(data_bloques_undet, itertools.repeat(dt_s),
-                             itertools.repeat(dtmax_s), itertools.repeat(tb))
-
-    return pool.map(wrapper_arossi_una_historia_I, argumentos_wrapper)
+    _num_proc = mp.cpu_count()
+    _pool = mp.Pool(processes=_num_proc)
+    print('Se utilizan {} procesos'.format(_num_proc))
+    print('-' * 50)
+    results_detectores = []  # Lista para los resultados de cada detector
+    # Itero sobre cada detector
+    for i, data_un_detector in enumerate(data_bloques):
+        print('Procesando al archivo [{}]'.format(i))
+        # Construyo el argumento del wrapper en forma de tupla
+        argumentos_wrapper = zip(data_un_detector, itertools.repeat(dt_s),
+                                 itertools.repeat(dtmax_s),
+                                 itertools.repeat(tb))
+        _res = _pool.map(wrapper_arossi_una_historia_I, argumentos_wrapper)
+        _res = np.asarray(_res)
+        results_detectores.append(_res)
+        print('-' * 50)
+    return results_detectores
 
 
 if __name__ == '__main__':
@@ -151,8 +190,8 @@ if __name__ == '__main__':
     # -------------------------------------------------------------------------
     # Archivos a leer
     nombres = [
-              '../datos/medicion04.a.inter.D1.bin',
-              '../datos/medicion04.a.inter.D2.bin',
+               '../datos/medicion04.a.inter.D1.bin',
+               '../datos/medicion04.a.inter.D2.bin',
               ]
     # Cantidad de historias
     Nhist = 100
@@ -162,29 +201,55 @@ if __name__ == '__main__':
     dtmax_s = 50e-3
 
     # -------------------------------------------------------------------------
-    # Para probar
+    # Para probar `arossi_historias()'
     # -------------------------------------------------------------------------
-    data_bloques, _, _ = alfa_rossi_preprocesado(nombres, Nhist, tb)
-
+    data_bloq, _, _ = alfa_rossi_preprocesado(nombres, Nhist, tb)
     # En serie
     """
     t0 = time.time()
     a_s = arossi_serial(data_bloques[0], dt_s, dtmax_s, tb)
     tf = time.time()
     print('Tiempo serie: {} s'.format(tf-t0))
+    quit()
     """
 
     # En paralelo
     t0 = time.time()
-    a_p = arossi_historias_undet(data_bloques[0], dt_s, dtmax_s, tb)
+    a_p = arossi_historias(data_bloq, dt_s, dtmax_s, tb)
     tf = time.time()
     print('Tiempo paralelo: {} s'.format(tf-t0))
-    print(a_p[50][0])
-    a_p = np.asarray(a_p)
-    print(a_p[50][0])
-    print(a_p[:, 1])
+
+    fig1, ax1 = plt.subplots(1, 1)
+    m = np.mean(a_p[0][:, 0])
+    ax1.plot(m)
+    m = np.mean(a_p[1][:, 0])
+    ax1.plot(m)
+
+    plt.show()
+    quit()
+    # Gráficos de triggers y tasa de cuentas para cada historia
+    fig1, axs = plt.subplots(2, 1, sharex='col')
+    hist = np.linspace(0, Nhist-1, Nhist, dtype=int)
+    for i, det in enumerate(a_p):
+        R = np.asarray(list(det[:, 1]))
+        lab_str = 'Archivo ' + str(i)
+        axs[0].errorbar(hist, R[:, 0], yerr=R[:, 1], fmt='.', elinewidth=0.5,
+                        lw=0, label=lab_str)
+        Trig = det[:, 2]
+        axs[1].plot(hist, Trig, '.', label=lab_str)
+
+    axs[0].set_ylabel('Tasa de cuentas [cps]')
+    axs[1].set_ylabel('# triggers')
+    axs[1].set_xlabel('Historias')
+
+    for ax in axs:
+        ax.grid(True)
+        ax.legend(loc='best')
+
+    plt.show()
+
     # -------------------------------------------------------------------------
-    # Para probar arossi_una_historia_I
+    # Para probar 'arossi_una_historia_I()'
     # -------------------------------------------------------------------------
     """
     data_bloques, _, _ = alfa_rossi_preprocesado(nombres, Nhist, tb)
