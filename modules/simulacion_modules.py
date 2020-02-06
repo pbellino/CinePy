331268c38156508353a8cd25800c6c18f7df494f
Pablo B <pbellino@gmail.com>
2020-02-06 06:38:37 -0300

    Se pasan funciones a archivo de módulos

--- modules/simulacion_modules.py
+++ modules/simulacion_modules.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from modules.alfa_rossi_procesamiento import arossi_una_historia_I
+from modules.io_modules import read_PTRAC_CAP_bin
 
 
 def agrega_tiempo_de_fuente(tasa, datos, filename):
@@ -83,6 +85,69 @@ def agrega_tiempo_de_fuente(tasa, datos, filename):
     np.savetxt(filename, times, fmt='%.12E')
 
     return times, cells, cells
+
+
+def RAD_sin_accidentales(nombre, dt_s, dtmax_s):
+    """
+    Función para obtener la distribución RAD a partir del PTRAC
+
+    El objetivo es comparar con la RAD obtenida con tallies F8 + GATES. Por
+    este motivo se deben eliminar las coincidencias accidentales.
+
+    Notar que pueden haber algunas diferencias porque la forma de obtener la
+    RAD con las tallies y con los algoritmos míos (arossi_una_historia_I) son
+    distintos. Yo obligo a que todos los triggers alcancen el 'dtmax_s'. Las
+    tallies F8 en cambio, abren un GATE para cualquier trigger.
+
+    Parámetros
+    ----------
+        nombre : string
+            Nombre del archivo binario PTRAC obtenido con event=CAP
+
+        dt_s : float (segundos)
+            Intervalo temporal para cada bin de la distribución
+
+        dtmax : float (segundos)
+            Máximo intervalo de la distribución de alfa-Rossi
+
+    Importante
+    ----------
+        Los valores dt_s y dtmax deben coincidir con los utilizados para
+        construir las tallies F8 con GATES sucesivas
+    """
+    # Leo el archivo binario de PTRAC con event=CAP
+    datos, header = read_PTRAC_CAP_bin(nombre)
+
+    # Convierto a array numpy
+    datos = np.asarray(datos)
+    # Números de historia
+    nps = np.asarray(datos[:, 0], dtype='int64')
+    # Tiempos
+    timestamp = np.asarray(datos[:, 1], dtype='float64') * 1e-8
+    # Obtiene los timestamp para una misma historia
+    times_historia = []
+    for n in set(nps):
+        indx = np.where(nps == n)
+        # Timestamp por historia y ordenado
+        list_by_hist = np.sort(timestamp[indx])
+        # Se fija t=0 en el primer pulso
+        list_by_hist -= list_by_hist[0]
+        times_historia.append(list_by_hist)
+
+    # Datos de la RAD
+    historias = []
+    for time in times_historia:
+        P_historia, _, N_trig, P_trig =  \
+            arossi_una_historia_I(time, dt_s, dtmax_s, 1)
+        # print(P_trig)
+        # Analizo los triggers para no tener que operar sobre P_historia
+        # (molesta la normalización en este caso)
+        if P_trig.size:
+            # Sumo todos trigger
+            historias.append(np.sum(P_trig, axis=0))
+    # Sumo entre todos los eventos para obtener la distribución de alfa-Rossi
+    RAD = np.sum(historias, axis=0)
+    return RAD
 
 
 if __name__ == "__main__":
--- simulacion/test_F8_RAD/RAD_from_PTRAC_Listmode.py
+++ simulacion/test_F8_RAD/RAD_from_PTRAC_Listmode.py
@@ -9,71 +9,8 @@ import numpy as np
 import sys
 
 sys.path.append('../../')
-from modules.alfa_rossi_procesamiento import arossi_una_historia_I
-from modules.io_modules import lee_tally_F8_RAD, read_PTRAC_CAP_bin
-
-
-def RAD_sin_accidentales(nombre, dt_s, dtmax_s):
-    """
-    Función para obtener la distribución RAD a partir del PTRAC
-
-    El objetivo es comparar con la RAD obtenida con tallies F8 + GATES. Por
-    este motivo se deben eliminar las coincidencias accidentales.
-
-    Notar que pueden haber algunas diferencias porque la forma de obtener la
-    RAD con las tallies y con los algoritmos míos (arossi_una_historia_I) son
-    distintos. Yo obligo a que todos los triggers alcancen el 'dtmax_s'. Las
-    tallies F8 en cambio, abren un GATE para cualquier trigger.
-
-    Parámetros
-    ----------
-        nombre : string
-            Nombre del archivo binario PTRAC obtenido con event=CAP
-
-        dt_s : float (segundos)
-            Intervalo temporal para cada bin de la distribución
-
-        dtmax : float (segundos)
-            Máximo intervalo de la distribución de alfa-Rossi
-
-    Importante
-    ----------
-        Los valores dt_s y dtmax deben coincidir con los utilizados para
-        construir las tallies F8 con GATES sucesivas
-    """
-    # Leo el archivo binario de PTRAC con event=CAP
-    datos, header = read_PTRAC_CAP_bin(archivo_bin)
-
-    # Convierto a array numpy
-    datos = np.asarray(datos)
-    # Números de historia
-    nps = np.asarray(datos[:, 0], dtype='int64')
-    # Tiempos
-    timestamp = np.asarray(datos[:, 1], dtype='float64') * 1e-8
-    # Obtiene los timestamp para una misma historia
-    times_historia = []
-    for n in set(nps):
-        indx = np.where(nps == n)
-        # Timestamp por historia y ordenado
-        list_by_hist = np.sort(timestamp[indx])
-        # Se fija t=0 en el primer pulso
-        list_by_hist -= list_by_hist[0]
-        times_historia.append(list_by_hist)
-
-    # Datos de la RAD
-    historias = []
-    for time in times_historia:
-        P_historia, _, N_trig, P_trig =  \
-            arossi_una_historia_I(time, dt_s, dtmax_s, 1)
-        # print(P_trig)
-        # Analizo los triggers para no tener que operar sobre P_historia
-        # (molesta la normalización en este caso)
-        if P_trig.size:
-            # Sumo todos trigger
-            historias.append(np.sum(P_trig, axis=0))
-    # Sumo entre todos los eventos para obtener la distribución de alfa-Rossi
-    RAD = np.sum(historias, axis=0)
-    return RAD
+from modules.io_modules import lee_tally_F8_RAD
+from modules.simulacion_modules import RAD_sin_accidentales
 
 
 if __name__ == '__main__':
