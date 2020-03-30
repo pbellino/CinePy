0993db09e04302318bfc0f7315ad677aa491d792
Pablo B <pbellino@gmail.com>
2020-03-30 20:58:39 -0300

    Se agrega la parte de openmp

--- simulacion/paralelizacion/lanza_corridas.py
+++ simulacion/paralelizacion/lanza_corridas.py
@@ -9,15 +9,60 @@ import os
 import random as rnd
 import subprocess
 
+
+def _modifica_slurm_serie(slurm_script, slurm_base, input_corrida):
+    # Genera script de slurm para cada corrida
+    with open(slurm_script, 'w') as f:
+        for line in slurm_base:
+            if line.startswith('#SBATCH -J'):
+                # Nombre del job
+                f.write('#SBATCH -J ' + input_corrida + '\t # Job Name \n')
+            elif line.startswith('param='):
+                # Nombre del input
+                f.write('param=\"ixr i=' + input_corrida + ' n=' +
+                        input_corrida + '.\"\n')
+            else:
+                f.write(line)
+    return None
+
+
+def _modifica_slurm_openmp(slurm_script, slurm_base, input_corrida,
+                           n_tasks):
+    # Genera script de slurm para cada corrida con OpenMP
+    with open(slurm_script, 'w') as f:
+        for line in slurm_base:
+            if line.startswith('#SBATCH -J'):
+                # Nombre del job
+                f.write('#SBATCH -J ' + input_corrida + '\t # Job Name \n')
+            elif line.startswith('#SBATCH -n'):
+                # Cantidad de cores
+                f.write('#SBATCH -n ' + n_tasks + '\t # Number of cpu cores\n')
+            elif line.startswith('param='):
+                # Nombre del input
+                f.write('param=\"ixr tasks ' + n_tasks + ' i=' +
+                        input_corrida + ' n=' + input_corrida + '.\"\n')
+            elif line.startswith('GOMP_DEBUG='):
+                # Corrida
+                f.write('GOMP_DEBUG=1 OMP_DISPLAY_ENV=VERBOSE OMP_NUM_THREADS='
+                        + n_tasks + ' $bin $param\n')
+            else:
+                f.write(line)
+    return None
+
+
 if __name__ == '__main__':
 
     ###########################################################################
     # Input parameters
     ###########################################################################
-    n_corridas = 8
+    n_corridas = 2
     input_mcnp = 'input'
-    slurm_script = 'mcnp6.slurm'  # Para un solo proceso en serie
+    slurm_script = 'mcnp6.slurm'  # Un solo proceso en serie
+    slurm_script = 'mcnp6_gcc6.3_openmp.slurm'  # Usando OpenMP
+    n_tasks = 32   # Sólo se utiliza para OpenMP
     ###########################################################################
+
+    n_tasks = str(n_tasks)
 
     # Directorio raiz
     parent = os.getcwd()
@@ -50,22 +95,18 @@ if __name__ == '__main__':
                 else:
                     f.write(line)
 
-        # Genera script de slurm para cada corrida
-        with open(slurm_script, 'w') as f:
-            for line in slurm_base:
-                if line.startswith('#SBATCH -J'):
-                    # Nombre del job
-                    f.write('#SBATCH -J ' + input_corrida + '\t # Job Name \n')
-                elif line.startswith('param='):
-                    # Nombre del input
-                    f.write('param=\"ixr i=' + input_corrida + ' n=' +
-                            input_corrida + '.\"\n')
-                else:
-                    f.write(line)
+        # Genera script se slurm
+        if 'openmp' in slurm_script:
+            _modifica_slurm_openmp(slurm_script, slurm_base, input_corrida,
+                                   n_tasks)
+        else:
+            _modifica_slurm_serie(slurm_script, slurm_base, input_corrida)
 
         # Se envia el job
         command = "sbatch " + slurm_script
         process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
         output, error = process.communicate()
+        
+        print(' Se sale de la carpeta ' + dir_corrida)
 
         os.chdir(parent)
