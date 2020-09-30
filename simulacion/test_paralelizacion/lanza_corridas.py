d0954ea470a056f9c5248c56b33db04f61fd6bd1
Pablo B <pbellino@gmail.com>
2020-09-30 17:08:39 -0300

    Sei incorpora uso de script para ptrac de n + p

--- simulacion/test_paralelizacion/lanza_corridas.py
+++ simulacion/test_paralelizacion/lanza_corridas.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-TODO
+Script que lanza en paralelo corridas de mcnp con distintas semillas
 """
 
 import sys
@@ -12,4 +12,4 @@ from modules.corridas_paralelizadas import run_paralelo
 if __name__ == '__main__':
 
     # Para correr en una PC
-    run_paralelo(4, 'in_sdef', machine='PC')
+    run_paralelo(4, 'in_sdef', machine='PC', run_script='run_sdef.sh')
