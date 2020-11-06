c25f370acd5a992416183221e98f8bb33848f182
Pablo Bellino <pbellino@gmail.com>
2020-11-06 13:56:32 -0300

    Ahora se guardan datos intermedios agrupados

--- simulacion/test_paralelizacion/recopila_datos.py
+++ simulacion/test_paralelizacion/recopila_datos.py
@@ -15,15 +15,18 @@ from modules.simulacion_modules import lee_nps_entrada, read_PTRAC_estandar, \
 
 if __name__ == '__main__':
 
+    nombre = 'case'
+
     # Corridas n + p
-    datos_n, datos_p, nps = merge_outputs(dos_corridas=True)
+    datos_n, datos_p, nps = merge_outputs(nombre=nombre, dos_corridas=True)
+    # Se leen los datos
     datos_n_fte = agrega_tiempo_de_fuente(100, nps, datos_n)
     datos_p_fte = agrega_tiempo_de_fuente(100, nps, datos_p)
     print(nps, np.shape(datos_n), np.shape(datos_p))
     print(np.shape(datos_n_fte), np.shape(datos_p_fte))
 
     # Corridas n
-    # datos_n, nps = merge_outputs()
+    # datos_n, nps = merge_outputs(nombre=nombre)
     # datos_n_fte = agrega_tiempo_de_fuente(100, nps, datos_n)
     # print(nps, np.shape(datos_n))
     # print(np.shape(datos_n_fte))
