5da4373dc1eb0816d7452fdd9235cfabd14f9737
Pablo Bellino <pbellino@gmail.com>
2021-01-10 12:39:42 -0300

    Se hacen todos los métodos a la vez

--- simulacion/cinetica_puntual/afey_analisis.py
+++ simulacion/cinetica_puntual/afey_analisis.py
@@ -12,7 +12,7 @@ from modules.alfa_feynman_analisis import teo_variance_berglof, grafica_afey, \
                                           ajuste_afey_nldtime, ajuste_afey, \
                                           grafica_historias_afey, lee_Nk, \
                                           ajuste_afey_2exp, ajuste_afey_3exp, \
-                                          ajuste_afey_delayed, \
+                                          ajuste_afey_delayed, grafica_Nk, \
                                           ajuste_afey_2exp_delayed, \
                                           teo_variance_berglof_exacta, \
                                           teo_variance_pacilio
@@ -27,20 +27,17 @@ if __name__ == '__main__':
     # Gráfico de las historias
     # -------------------------------------------------------------------------
     # nombre = 'resultados_afey/times.D1_var_mca.dat'
-    # #nombre = 'resultados_afey/times.D1_var.dat'
-    # #nombre = 'resultados_afey/times.D1_var.dat'
-    # nombre = 'resultados_afey/times.D1_var_choice.dat'
-    # nombre = 'resultados_afey/times.D1_var_skip.dat'
     # grafica_historias_afey(nombre)
     # plt.show()
     # quit()
+    folder = 'resultados_afey'
 
-    nombres = [
-     #         'resultados_afey/times.D1_var.fey',
-     #         'resultados_afey/times.D1_var_choice.fey',
-               'resultados_afey/times.D1_var_skip.fey',
-     #         'resultados_afey/times.D1_var_mca.fey',
-              ]
+    nombres = {
+            'bounching': folder + '/times.D1_var.fey',
+               'choice': folder + '/times.D1_var_choice.fey',
+                 'skip': folder + '/times.D1_var_skip.fey',
+                  'mca': folder + '/times.D1_var_mca.fey',
+              }
     # -------------------------------------------------------------------------
     #
     # myfig = grafica_afey(nombres)
@@ -50,38 +47,70 @@ if __name__ == '__main__':
     # plt.show()
     #
     # ------------------------------------------------------------------------
-    nombre = nombres[0]
-    # Camino absoluto del archivo que se quiere leer
-    abs_nombre = os.path.join(script_dir, nombre)
-    tau, Y, std_Y, num_hist, tasas = lee_fey(abs_nombre)
-    print(std_Y / Y * 100)
-    Nk = lee_Nk(nombre.rstrip('fey') + 'Nk')
-    var_teo = teo_variance_berglof(Y, Nk)
-    var_teo_exac = teo_variance_berglof_exacta(Y, Nk, tasas[0], tau)
-    var_teo_pacilio = teo_variance_pacilio(Y, Nk, tasas[0], tau)
 
-    _, val, teo = ajuste_afey(tau, Y, std_Y, [400, 10, 0], vary=[1, 1, 0],
-                               Nk=Nk)
-    #
-    #_, val, teo = ajuste_afey_delayed(tau, Y, std_Y, [500, 1, 0, 1],
-    #                                    vary=[1, 1, 0, 1])
-    print(2*'\n')
-    [print("Estimados: {:.4e} - Teóricos: {:.4e}".format(va, te))
-            for va, te in zip(val, teo)]
-    # ajuste_afey_2exp(tau, Y, std_Y)
-    # ajuste_afey_3exp(tau, Y, std_Y)
-    # ajuste_afey_2exp_delayed(tau, Y, std_Y, [0.5, 1, 0, 3, 1, 1])
-    # ajuste_afey_nldtime(tau, Y, std_Y)
+    Nk_res = {}
+    results = {}
+    for key, nombre in nombres.items():
+        print(key,nombre)
 
-    fig8, ax8 = plt.subplots(1, 1)
-    ax8.plot(tau, num_hist*std_Y**2, '.', label='Estimated variance')
-    ax8.set_xlabel(r'$\tau$ [s]')
-    ax8.set_ylabel(r'Var[Y($\tau$)]')
+        # Camino absoluto del archivo que se quiere leer
+        abs_nombre = os.path.join(script_dir, nombre)
+        tau, Y, std_Y, num_hist, tasas = lee_fey(abs_nombre)
+        # Puntos para cada tau por batch
+        Nk = lee_Nk(nombre.rstrip('fey') + 'Nk')
 
-    ax8.plot(tau, var_teo, 'r', lw=2, label='Teoretical variance')
-    ax8.plot(tau, var_teo_exac, 'k', lw=2, label='Teoretical variance exact')
-    ax8.plot(tau, var_teo_pacilio, 'g', lw=2, label='Teoretical variance pacilio')
-    ax8.grid(True)
-    ax8.legend(loc='best')
+        _, val, teo = ajuste_afey(tau, Y, std_Y, [400, 10, 0], vary=[1, 1, 0],
+                                  Nk=Nk, tasa=tasas, verbose=False, plot=False,
+                                  conf_int=False)
+        results[key] = val
+
+        Nk_res[key] = (tau, Nk)
+        #
+        #_, val, teo = ajuste_afey_delayed(tau, Y, std_Y, [500, 1, 0, 1],
+        #                                    vary=[1, 1, 0, 1])
+        print(2*'\n')
+        [print("Estimados: {:.4e} - Teóricos: {:.4e}".format(va, te))
+                for va, te in zip(val, teo)]
+        # ajuste_afey_nldtime(tau, Y, std_Y)
+
+        # Análisis de incertezas
+        if False:
+            var_teo_exac = teo_variance_berglof_exacta(Y, Nk, tasas[0], tau)
+            rel_teo_exac = 100 * np.sqrt(var_teo_exac / num_hist) / Y
+            var_teo_pacilio = teo_variance_pacilio(Y, Nk, tasas[0], tau)
+            rel_teo_pacilio = 100 * np.sqrt(var_teo_pacilio / num_hist) / Y
+
+            fig8, ax8 = plt.subplots(1, 1)
+            ax8.plot(tau, 100 * std_Y / Y, '.', label='Estimated')
+            ax8.set_xlabel(r'$\tau$ [s]]')
+            ax8.set_ylabel(r'$\sigma_{\bar Y}/\bar Y (\tau)$ [%]')
+
+            ax8.plot(tau, rel_teo_exac, 'k', lw=2, label='Ordendorf')
+            ax8.plot(tau, rel_teo_pacilio, 'g', lw=2, label='Pacilio')
+            ax8.grid(True)
+            ax8.legend(loc='best')
+
+            plt.show()
+
+    fig, axs = plt.subplots(3, 1, sharex=True)
+    for key, result in results.items():
+        for res, ax in zip(result, axs):
+            ax.errorbar([key], res.n, yerr=res.s, fmt='s')
+    varis = [r"$\alpha$ [1/s]", r"$\epsilon$", r"$F$ [1/s]"]
+
+    # Gráfico valores teóricos
+    x_teo = np.linspace(-0.1, len(result)+0.1, 100)
+    y_teo = np.ones(np.shape(x_teo))
+    for ax, var, te in zip(axs, varis, teo):
+        ax.set_ylabel(var)
+        ax.plot(x_teo, te * y_teo, 'k', lw=2)
+        # ax.set_ylim(te * 0.95, te * 1.15)
+
+    plt.tight_layout()
+
+    # Gráfica de Nk vs tau
+    grafica_Nk(Nk_res)
 
     plt.show()
+
+
