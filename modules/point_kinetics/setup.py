d4f0a9e039902fc6995caea690be993a575d1c83
Pablo Bellino <pbellino@gmail.com>
2021-09-30 13:12:51 -0300

    Se agragan otras funciones para ser compiladas

--- modules/point_kinetics/setup.py
+++ modules/point_kinetics/setup.py
@@ -4,5 +4,5 @@ from Cython.Build import cythonize
 """ Script para compilar con cython """
 
 setup(
-    ext_modules = cythonize("reactimeter.pyx")
+    ext_modules = cythonize(["reactimeter.pyx", "direct_kinetic_solver.pyx"])
 )
