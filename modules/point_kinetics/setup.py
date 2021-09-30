681b0ca4548c3382ed48bb8dced842cee6b33043
Pablo Bellino <pbellino@gmail.com>
2021-09-30 13:59:29 -0300

    Ahora el setup.py se genera con el archivo en bash

--- modules/point_kinetics/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-""" Script para compilar con cython """
-
-setup(
-    ext_modules = cythonize(["reactimeter.pyx", "direct_kinetic_solver.pyx"])
-)
