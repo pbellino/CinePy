d37baa17ed409f6808196db62cec29e7774d450f
Pablo Bellino <pbellino@gmail.com>
2021-09-30 13:59:51 -0300

    Archivo para compilar los módulos en cython

---
+++ modules/point_kinetics/compile_cython.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Auxiliary script used for compiling .pyx files with cython
+# Also: copy .so files and delete temporal folders
+
+
+FILES=(reactimeter direct_kinetic_solver)
+
+for file in "${FILES[@]}"; do
+    cat > setup.py << EOF
+from distutils.core import setup
+from Cython.Build import cythonize
+
+
+setup(
+    ext_modules = cythonize("${file}.pyx")
+)
+EOF
+
+    python3 setup.py build_ext --inplace
+    cp modules/point_kinetics/${file}*.so ./${file}.so
+    rm ${file}.c
+done
+
+rm -fr build modules
+
