d15a5d814e0cc961f3642cc8be182c1236e08bfe
Pablo Bellino <pbellino@gmail.com>
2021-10-01 12:31:28 -0300

    Función para resolver cinetica directa con scipy.integrate.ode

---
+++ modules/point_kinetics/soluciones_numericas.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from scipy.integrate import ode
+
+
+def direct_pk_ODE_solver(rho, n0, dt, tmax, constants, source):
+    """
+    Solver for the direct point kinetic equation using the scipy.ingegrate.ode
+    solver.
+
+    Parameters
+    ----------
+        rho : callable (returns float)
+            Reactivity as a function of time (in dolars)
+        n0 : float
+            Initial value of the neutron density
+        dt : float
+            Time step
+        tmax : float
+            Maximun integration time
+        constants : touple or list (b, lambda, L*)
+            b (list), lambda (list), reduced Lambda (float)
+            b_i = beta_i / beta_eff
+            L* = L/beta_eff
+        source : callable (returns array)
+            Strength of the neutron sources as a function of time.
+            The length of the returned array should match the number of
+            delayed neutron precursors plus one
+
+    Returns
+    -------
+        t_ode : numpy array
+            Times
+        n_ode : numpy array
+            Neutron density
+        c_ode : numpy ndarray
+            Delayed neutron precursors
+
+    >>> def rho(t):
+    >>>    if t>= 2:
+    >>>       return rho_f
+    >>>    elif t<= 2:
+    >>>       return rho_i
+    >>>
+    >>> def S(t):
+    >>>     _vect = np.zeros(len(b) +1 )
+    >>>     if  True:
+    >>>         _vect[0]= 0
+    >>>     return _vect
+
+    """
+
+
+    if source(0)[0] == 0.0:
+        if rho(0) != 0:
+            _msg = "Advertencia: la única solución estacionaria para un \n"
+            _msg += "sin fuente y no crítico es n=0"
+            print(_msg)
+            n0 = 0
+        else:
+            n0 = 5
+    else:
+        if rho(0) >= 0:
+            _msg = "Error: el reactor no se encuentra inicialmente \n"
+            _msg += " estacionario (S!=0 y rho>=0)"
+            raise Exception(_msg)
+        else:
+            n0 = -Lambda_red * source(0)[0] / rho(0)
+
+    b, lam, Lambda_red = constants
+
+    def func_ode(t, X, P, U):
+        # Y = A*X + S
+        P[0, 0] = (rho(t) - 1) / Lambda_red
+        return np.asarray(np.matmul(P, X) + U(t))
+
+    A = np.zeros((7, 7))
+    A[1:, 1:] = np.diag(-lam)
+    A[0, 1: ] = lam
+    A[1:, 0] = b / Lambda_red
+
+    X0 = b / lam / Lambda_red * n0
+    X0 = np.insert(X0, 0, n0)
+
+    # ------ Se resuelve utilizando scipy.integrate.ode
+    r = ode(func_ode)
+    r.set_integrator('dopri5', max_step=dt)
+    r.set_initial_value(X0, 0.0)
+    r.set_f_params(A, source)
+
+    t_ode = []
+    y_ode = []
+    while r.successful() and r.t < tmax:
+        t_ode.append(r.t)
+        y_ode.append(r.y)
+        r.integrate(r.t+dt)
+
+    # print(f"Return code : {r.get_return_code()}")
+
+    t_ode = np.asarray(t_ode)
+    n_ode = np.asarray(y_ode)[:, 0]
+    c_ode = np.asarray(y_ode)[:, 1:]
+
+    return t_ode, n_ode, c_ode
+
+
+if __name__ == "__main__":
+    pass
